25072864c977f6d39a03d2754694f73bb34af132
Muhammad ismaeel <muhammad.ismaeel5162@gmail.com>
2023-08-29 11:57:05 +0500

    ss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Made  by  Muhammad Ismaeel</h3>
+          <h3>Made  by  Bilal Ahmad</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} MI</h3>
+          <h3>Copyright © {year} BI</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
